456e82577d5a024e1abf298e3907c262404e7757
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-06-08 13:22:54 +0300

    django projects

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+         'post': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-     return render(request, 'about/home.html')
+     return render(request, 'about/home.html', {'title': 'About'})
 
